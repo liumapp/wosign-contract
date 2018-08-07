c125775fa695cac4d5b4a01bfa45632d297f37b2
liumapp <liumapp.com@gmail.com>
2018-08-07 14:10:24 +0800

    rm uncessary things

---
+++ src/main/java/com/liumapp/demo/wosign/WosignContractMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.wosign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file WosignContractMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/7
+ */
+@SpringBootApplication
+public class WosignContractMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WosignContractMain.class , args);
+    }
+
+}
