bc1a1e4a830dfbce149c95b743dc5ae6b3ff22a3
liumapp <liumapp.com@gmail.com>
2018-08-07 14:33:39 +0800

    ready to open a file and convert to base64

---
+++ src/test/java/com/liumapp/demo/wosign/UtilTest.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.wosign;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @author liumapp
+ * @file UtilTest.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/7
+ */
+public class UtilTest extends TestCase {
+
+    @Test
+    public void testOpenFileAndConvertToBase64 () {
+        
+    }
+
+}
