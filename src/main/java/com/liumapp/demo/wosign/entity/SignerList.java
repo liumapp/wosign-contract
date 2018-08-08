4a61ddaf8e3eae05d46943ce4cbbd20a216946ba
liumapp <liumapp.com@gmail.com>
2018-08-08 11:03:49 +0800

    ready to test

--- src/main/java/com/liumapp/demo/wosign/entity/SignerList.java
+++ src/main/java/com/liumapp/demo/wosign/entity/SignerList.java
@@ -2,6 +2,8 @@ package com.liumapp.demo.wosign.entity;
 
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+
 /**
  * @author liumapp
  * @file SignerList.java
@@ -12,18 +14,120 @@ import org.springframework.stereotype.Component;
 @Component
 public class SignerList {
 
-    //企业用户
-    private String signType; //企业用户为1
+    private String signType; //企业用户为1 个人用户为2
 
+    //企业用户
     private String orgName;//企业名称
 
     private String orgNo; //营业执照号
 
+    //个人用户
+    private String userName;
+
+    private String userNo;
+
+    //通用
+    private String pic; //营业执照照片base64 个人用户为身份证正面照片base64
+
+    private String otherpic; //开户许可证照片base64 个人用户为身份证反面照片base64
+
     private String stampbase;//签章base64，可传可不传
 
-    private String pic; //营业执照照片base64
+    private LinkedList<SignList> signlist;
 
-    private String otherpic; //开户许可证照片base64
+    public SignerList() {
+    }
 
+    public SignerList(String signType, String orgName, String orgNo, String userName, String userNo, String pic, String otherpic, String stampbase, LinkedList<SignList> signlist) {
+        this.signType = signType;
+        this.orgName = orgName;
+        this.orgNo = orgNo;
+        this.userName = userName;
+        this.userNo = userNo;
+        this.pic = pic;
+        this.otherpic = otherpic;
+        this.stampbase = stampbase;
+        this.signlist = signlist;
+    }
 
+    public String getSignType() {
+        return signType;
+    }
+
+    public SignerList setSignType(String signType) {
+        this.signType = signType;
+        return this;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public SignerList setOrgName(String orgName) {
+        this.orgName = orgName;
+        return this;
+    }
+
+    public String getOrgNo() {
+        return orgNo;
+    }
+
+    public SignerList setOrgNo(String orgNo) {
+        this.orgNo = orgNo;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public SignerList setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getUserNo() {
+        return userNo;
+    }
+
+    public SignerList setUserNo(String userNo) {
+        this.userNo = userNo;
+        return this;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public SignerList setPic(String pic) {
+        this.pic = pic;
+        return this;
+    }
+
+    public String getOtherpic() {
+        return otherpic;
+    }
+
+    public SignerList setOtherpic(String otherpic) {
+        this.otherpic = otherpic;
+        return this;
+    }
+
+    public String getStampbase() {
+        return stampbase;
+    }
+
+    public SignerList setStampbase(String stampbase) {
+        this.stampbase = stampbase;
+        return this;
+    }
+
+    public LinkedList<SignList> getSignlist() {
+        return signlist;
+    }
+
+    public SignerList setSignlist(LinkedList<SignList> signlist) {
+        this.signlist = signlist;
+        return this;
+    }
 }
--- src/test/java/com/liumapp/demo/wosign/HttpsConnUtilsTest.java
+++ src/test/java/com/liumapp/demo/wosign/HttpsConnUtilsTest.java
@@ -1,11 +1,18 @@
 package com.liumapp.demo.wosign;
 
+import com.alibaba.fastjson.JSON;
+import com.liumapp.demo.wosign.entity.SignList;
+import com.liumapp.demo.wosign.entity.SignerList;
 import com.liumapp.demo.wosign.utils.HttpsConnUtils;
+import com.liumapp.qtools.file.base64.Base64FileTool;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedList;
 
 /**
  * @author liumapp
@@ -18,12 +25,29 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HttpsConnUtilsTest {
 
+    @Value("${custom.picPath}")
+    private String picPath;
+
     @Autowired
     private HttpsConnUtils httpsConnUtils;
 
     @Test
     public void testSign () throws Exception {
-        httpsConnUtils.post("https://docapitest.wosign.com", null);
+        SignerList signerList = new SignerList();
+        SignList signList = new SignList();
+        LinkedList<SignList> lists = new LinkedList<>();
+        signList.setPagenumber(1)
+                .setPosition("0.30,0.57,0.46,0.69");
+        lists.add(signList);
+        signerList.setSignType("2")
+                .setUserName("zhangsan")
+                .setUserNo("123456789123456789")
+                .setPic(Base64FileTool.filePathToBase64(picPath + "1.jpg"))
+                .setOtherpic(Base64FileTool.filePathToBase64(picPath + "2.jpg"))
+                .setSignlist(lists);
+        System.out.println("send info : ");
+        System.out.println(JSON.toJSONString(signerList));
+        httpsConnUtils.post("https://docapitest.wosign.com", JSON.toJSONString(signerList));
     }
 
 }
