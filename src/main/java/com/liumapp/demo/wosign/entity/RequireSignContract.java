ac837a7a24f33261b76b7eb875d4228895113a68
liumapp <liumapp.com@gmail.com>
2018-08-08 15:15:58 +0800

    test success

--- src/main/java/com/liumapp/demo/wosign/entity/RequireSignContract.java
+++ src/main/java/com/liumapp/demo/wosign/entity/RequireSignContract.java
@@ -2,6 +2,8 @@ package com.liumapp.demo.wosign.entity;
 
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+
 /**
  * @author liumapp
  * @file RequireSignContract.java
@@ -14,7 +16,7 @@ public class RequireSignContract {
 
     private String tokenid;
 
-    private String actiontype;
+    private String actiontype; //CreateSignOrder
 
     private String filebase;
 
@@ -24,9 +26,105 @@ public class RequireSignContract {
 
     private String tempid; //模板id
 
-    private String suject; //签署合同标签
+    private String subject; //签署合同标签
 
     private String password;
 
+    private LinkedList<SignerList> signerlist;
 
+    public RequireSignContract() {
+    }
+
+    public RequireSignContract(String tokenid, String actiontype, String filebase, String fileformat, String orderNumber, String tempid, String subject, String password, LinkedList<SignerList> signerlist) {
+        this.tokenid = tokenid;
+        this.actiontype = actiontype;
+        this.filebase = filebase;
+        this.fileformat = fileformat;
+        this.orderNumber = orderNumber;
+        this.tempid = tempid;
+        this.subject = subject;
+        this.password = password;
+        this.signerlist = signerlist;
+    }
+
+    public String getTokenid() {
+        return tokenid;
+    }
+
+    public RequireSignContract setTokenid(String tokenid) {
+        this.tokenid = tokenid;
+        return this;
+    }
+
+    public String getActiontype() {
+        return actiontype;
+    }
+
+    public RequireSignContract setActiontype(String actiontype) {
+        this.actiontype = actiontype;
+        return this;
+    }
+
+    public String getFilebase() {
+        return filebase;
+    }
+
+    public RequireSignContract setFilebase(String filebase) {
+        this.filebase = filebase;
+        return this;
+    }
+
+    public String getFileformat() {
+        return fileformat;
+    }
+
+    public RequireSignContract setFileformat(String fileformat) {
+        this.fileformat = fileformat;
+        return this;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public RequireSignContract setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+        return this;
+    }
+
+    public String getTempid() {
+        return tempid;
+    }
+
+    public RequireSignContract setTempid(String tempid) {
+        this.tempid = tempid;
+        return this;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public RequireSignContract setSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public RequireSignContract setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public LinkedList<SignerList> getSignerlist() {
+        return signerlist;
+    }
+
+    public RequireSignContract setSignerlist(LinkedList<SignerList> signerlist) {
+        this.signerlist = signerlist;
+        return this;
+    }
 }
--- src/test/java/com/liumapp/demo/wosign/HttpsConnUtilsTest.java
+++ src/test/java/com/liumapp/demo/wosign/HttpsConnUtilsTest.java
@@ -1,17 +1,23 @@
 package com.liumapp.demo.wosign;
 
 import com.alibaba.fastjson.JSON;
+import com.liumapp.demo.wosign.entity.RequireSignContract;
 import com.liumapp.demo.wosign.entity.SignList;
 import com.liumapp.demo.wosign.entity.SignerList;
 import com.liumapp.demo.wosign.utils.HttpsConnUtils;
 import com.liumapp.qtools.file.base64.Base64FileTool;
+import com.liumapp.qtools.file.basic.FileTool;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.net.URLEncoder;
 import java.util.LinkedList;
 
 /**
@@ -28,14 +34,30 @@ public class HttpsConnUtilsTest {
     @Value("${custom.picPath}")
     private String picPath;
 
+    @Value("${custom.tokenid}")
+    private String tokenid;
+
+    @Value("${custom.pdfPath}")
+    private String pdfPath;
+
     @Autowired
     private HttpsConnUtils httpsConnUtils;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     public void testSign () throws Exception {
+        RequireSignContract requireSignContract = new RequireSignContract();
+        requireSignContract.setTokenid(tokenid)
+                .setActiontype("CreateSignOrder")
+                .setFilebase(Base64FileTool.filePathToBase64(pdfPath + "test.pdf"))
+                .setFileformat("pdf")
+                .setOrderNumber("wiejfisdjfiwe12312314")
+                .setSubject("测试合同");
         SignerList signerList = new SignerList();
         SignList signList = new SignList();
         LinkedList<SignList> lists = new LinkedList<>();
+        LinkedList<SignerList> signerLists = new LinkedList<>();
         signList.setPagenumber(1)
                 .setPosition("0.30,0.57,0.46,0.69");
         lists.add(signList);
@@ -45,9 +67,13 @@ public class HttpsConnUtilsTest {
                 .setPic(Base64FileTool.filePathToBase64(picPath + "1.jpg"))
                 .setOtherpic(Base64FileTool.filePathToBase64(picPath + "2.jpg"))
                 .setSignlist(lists);
-        System.out.println("send info : ");
-        System.out.println(JSON.toJSONString(signerList));
-        httpsConnUtils.post("https://docapitest.wosign.com", JSON.toJSONString(signerList));
+        signerLists.add(signerList);
+        requireSignContract.setSignerlist(signerLists);
+        logger.info("send info : ");
+        logger.info(JSON.toJSONString(requireSignContract));
+        FileTool.writeStringToNewFile(new File(pdfPath + "params.json"), JSON.toJSONString(requireSignContract));
+        String result = httpsConnUtils.post("https://docapitest.wosign.com", URLEncoder.encode(JSON.toJSONString(requireSignContract), "UTF-8"));
+        System.out.println(result);
     }
 
 }
